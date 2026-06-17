2e2ae6d7cf18e504b82c4624c155e63265795023
Huraira <raohuraira331.rb@gmail.com>
2026-06-17 14:14:13 -0700

    Refactor ConsoleScreen to utilize AppCard for improved UI structure
    - Replaced the Container widget with AppCard to enhance the layout and user experience.
    - Updated the console description and title for better context and clarity.

--- lib/features/console/views/console_screen.dart
+++ lib/features/console/views/console_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fluship/shared/widgets/app_card.dart';
 import 'package:flutter/material.dart';
 
 class ConsoleScreen extends StatelessWidget {
@@ -5,11 +6,10 @@ class ConsoleScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.sizeOf(context);
-    return Container(
-      color: Colors.blue,
-      height: size.height,
-      width: size.width,
+    return const AppCard(
+      description: 'Run commands in the console',
+      title: 'Console',
+      children: [],
     );
   }
 }
