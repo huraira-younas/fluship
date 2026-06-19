177b3deb50def1914baa7ee99c7fa4c76c302063
Huraira <raohuraira331.rb@gmail.com>
2026-06-19 15:00:47 -0700

    Add report generation and email functionality
    - Introduced a new report.py file to handle the generation of HTML reports and email notifications.
    - Implemented functions for creating HTML headers, footers, and summary rows to ensure consistent branding.
    - Added functionality to send emails with optional attachments using Gmail SMTP.
    - Included a method to save logs with timestamps for better tracking of build processes.
    - Enhanced the report generation with detailed step results and status badges for improved clarity in build outcomes.

--- lib/features/config/views/config_screen.dart
+++ lib/features/config/views/config_screen.dart
@@ -1,7 +1,7 @@
 import 'package:fluship/core/app_theme/fluship_theme_extension.dart';
-import 'package:fluship/shared/extensions/context_extensions.dart';
-import 'package:fluship/shared/extensions/widget_extensions.dart';
-import 'package:fluship/shared/widgets/app_cta_button.dart';
+// import 'package:fluship/shared/extensions/context_extensions.dart';
+// import 'package:fluship/shared/extensions/widget_extensions.dart';
+// import 'package:fluship/shared/widgets/app_cta_button.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 
@@ -14,36 +14,36 @@ class ConfigScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final spacing = context.flushipTheme.spacing;
-    final pad = context.screenHeight * 0.29;
+    // final pad = context.screenHeight * 0.29;
 
     return BlocBuilder<ConfigBloc, ConfigState>(
       builder: (context, state) {
-        if (state.loading) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          ).padOnly(t: pad * 1.35);
-        }
+        // if (state.loading) {
+        //   return const Center(
+        //     child: CircularProgressIndicator(),
+        //   ).padOnly(t: pad * 1.35);
+        // }
 
-        if (state.error != null) {
-          return AppCtaButton(
-            onTap: () => context.read<ConfigBloc>().add(const LoadConfig()),
-            text: state.error!.message,
-            icon: Icons.error,
-            btnText: "Retry",
-            title: "Error",
-          ).padOnly(t: pad);
-        }
+        // if (state.error != null) {
+        //   return AppCtaButton(
+        //     onTap: () => context.read<ConfigBloc>().add(const LoadConfig()),
+        //     text: state.error!.message,
+        //     icon: Icons.error,
+        //     btnText: "Retry",
+        //     title: "Error",
+        //   ).padOnly(t: pad);
+        // }
 
-        final fp = state.appInfo.flutterProjectPath;
-        if (fp == null || fp.isEmpty) {
-          return AppCtaButton(
-            onTap: () => context.read<ConfigBloc>().add(const LoadConfig()),
-            text: "No Flutter project path found",
-            btnText: "Select Project",
-            icon: Icons.folder,
-            title: "Error",
-          ).padOnly(t: pad);
-        }
+        // final fp = state.appInfo.flutterProjectPath;
+        // if (fp == null || fp.isEmpty) {
+        //   return AppCtaButton(
+        //     onTap: () => context.read<ConfigBloc>().add(const LoadConfig()),
+        //     text: "No Flutter project path found",
+        //     btnText: "Select Project",
+        //     icon: Icons.folder,
+        //     title: "Error",
+        //   ).padOnly(t: pad);
+        // }
 
         return Column(
           spacing: spacing.md,
